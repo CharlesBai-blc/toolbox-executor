eb05e3cdcaec9a3889e7d563bcac4d9286d304db
CharlesBai-blc <cb06blc@gmail.com>
2026-01-07 15:37:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '/home/ubuntu/executor/src/.env' });
 const express = require('express');
 const { exec } = require('child_process');
 const fs = require('fs/promises');
